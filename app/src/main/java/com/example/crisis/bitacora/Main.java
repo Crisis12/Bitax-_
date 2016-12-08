13e7b6b51297b82606f6b71bf458f2787189f24f
Crisis <cggm.r12@gmail.com>
2016-12-08 09:28:25 -0600

    shalalalalala~

---
+++ app/src/main/java/com/example/crisis/bitacora/Main.java
@@ -0,0 +1,46 @@
+package com.example.crisis.bitacora;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnAsistencia();
+        btnVehiculos();
+    }
+
+    public void btnAsistencia() {
+
+        ImageView entry = (ImageView) findViewById(R.id.btnAsistencia);
+
+        entry.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent actAsistencia = new Intent(getApplicationContext(), Asistencia.class);
+                startActivity(actAsistencia);
+            }
+        });
+    }
+
+    public void btnVehiculos() {
+
+        ImageView entry = (ImageView) findViewById(R.id.btnVehiculo);
+
+        entry.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent actVehiculo= new Intent(getApplicationContext(), Vehiculos.class);
+                startActivity(actVehiculo);
+            }
+        });
+    }
+}
